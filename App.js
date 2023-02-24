93132f0bed14fb4b65b022a52a527cfe198abfda
BaringM <84273184+BaringM@users.noreply.github.com>
2023-02-24 18:03:22 +0000

    got bottom three pages and two pages half working

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Ionicons } from '@expo/vector-icons';
 import { NavigationContainer } from '@react-navigation/native';
+// import { createStackNavigator } from '@react-navigation/stack';
 
+// const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 
 function ProfileScreen() {
@@ -15,10 +17,27 @@ function ProfileScreen() {
   );
 }
 
-function FeedScreen() {
+const FeedScreen = () => {
   return (
     <View style={styles.container}>
       <Text>Feed Screen</Text>
+    </View>
+  );
+};
+
+function FeedScreenWrapper({ navigation }) {
+  return (
+    <View style={{ flex: 1 }}>
+      <View style={styles.feedHeader}>
+        <TouchableOpacity onPress={() => navigation.navigate('Settings')}>
+          <Ionicons name="settings-outline" size={24} color="white" />
+        </TouchableOpacity>
+        <Text style={styles.headerText}>Feed</Text>
+        <TouchableOpacity onPress={() => navigation.navigate('Filters')}>
+          <Ionicons name="options-outline" size={24} color="white" />
+        </TouchableOpacity>
+      </View>
+      <FeedScreen />
     </View>
   );
 }
@@ -27,6 +46,28 @@ function ChatScreen() {
   return (
     <View style={styles.container}>
       <Text>Chat Screen</Text>
+    </View>
+  );
+}
+
+function SettingsScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text>Settings Screen</Text>
+      <TouchableOpacity onPress={() => navigation.goBack()}>
+        <Text>Go Back</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+function FiltersScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text>Filters Screen</Text>
+      <TouchableOpacity onPress={() => navigation.goBack()}>
+        <Text>Go Back</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -56,19 +97,54 @@ const App = () => {
         })}
       >
         <Tab.Screen name="Profile" component={ProfileScreen} />
-        <Tab.Screen name="Feed" component={FeedScreen} />
+        <Tab.Screen name="Feed" component={FeedScreenWrapper} />
         <Tab.Screen name="Chat" component={ChatScreen} />
       </Tab.Navigator>
+      {/* <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Settings" component={SettingsScreen} />
+          <Stack.Screen name="Filters" component={FiltersScreen} />
+        </Stack.Navigator>
+      </NavigationContainer> */}
     </NavigationContainer>
   );
-}
+};
 
 const styles = StyleSheet.create({
+
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center', 
+    justifyContent: 'center',
+  },
+  header: {
+    backgroundColor: 'tomato',
+    height: 80,
+    elevation: 0,
+    borderBottomWidth: 0,
+  },
+  headerTitle: {
+    color: 'white',
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  headerButtonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  headerButton: {
+    padding: 8,
+  },
+  headerButtonText: {
+    fontSize: 16,
+    color: '#555',
+  },
+  content: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   tabBar: {
     backgroundColor: '#fff',
@@ -78,6 +154,16 @@ const styles = StyleSheet.create({
   tabBarText: {
     fontSize: 10,
     color: '#666',
+  },
+  feedHeader: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    backgroundColor: 'grey',
+    height: 80,
+    paddingHorizontal: 16,
+    elevation: 0,
+    borderBottomWidth: 0,
   },
 });
 
